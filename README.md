2deffd1272c249dd5b86451d3be2d95ec3b782d7
Finn Jesse Dan <jessedan160@gmail.com>
2023-07-12 15:49:10 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# chat_blue
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/chat_blue/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.chat_blue
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
